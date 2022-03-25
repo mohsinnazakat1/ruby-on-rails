1f25b2098f2d91c9da87a1567a1fb8d961e1a49c
mohsinnazakat1 <csworld7177@gmail.com>
2022-03-25 13:10:31 +0500

    adding code to hello.rb

---
+++ introduction/helloWorld/hello.rb
@@ -0,0 +1,21 @@
+#hello world in ruby
+
+puts "Hello world" # put string does not return anything reuturn value is nil 
+
+#another way of writing hello world 
+
+p "Hello World" #this way returns something 
+
+print ("Hello world \n") #just doesn't go to next line 
+
+#defination of function that prints Hello world 
+def say_hello (text)
+    puts text
+end
+
+#defining function to say hello world 
+text_to_print = "Hello Mohsin"
+say_hello(text_to_print)
+
+
+
